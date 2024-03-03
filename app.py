acd050a959659be465d2f8ee6c2c2a020a5e3b27
Sumit Watkar <watkar.sumit@gmail.com>
2024-03-03 15:49:29 +0530

    cicd added

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def predict():
         raise CustomException(e, sys)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=8080, debug=True)
